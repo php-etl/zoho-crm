a4cfb28507d98270aa14b8f9b8b5d2524439ffa2
sebprt <sebastien@kiboko.fr>
2022-08-03 17:33:55 +0200

    Initial commit

---
+++ src/ContactLoader.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+use Kiboko\Contract\Pipeline\LoaderInterface;
+
+final class ContactLoader implements LoaderInterface
+{
+    public function load(): \Generator
+    {
+        $token = new \com\zoho\api\authenticator\OAuthToken(
+            getenv('ZOHO_CLIENT_ID'),
+            getenv('ZOHO_CLIENT_SECRET'),
+            getenv('ZOHO_ACCESS_TOKEN'),
+            getenv('ZOHO_REFRESH_TOKEN'),
+        );
+
+        $config = (new \com\zoho\crm\api\SDKConfigBuilder())->build();
+
+        \com\zoho\crm\api\Initializer::initialize(
+            new \com\zoho\crm\api\UserSignature(
+                getenv('ZOHO_USER_EMAIL'),
+            ),
+            \com\zoho\crm\api\dc\EUDataCenter::PRODUCTION(),
+            $token,
+            new \com\zoho\api\authenticator\store\FileStore('/tmp/zoho_token.txt'),
+            $config,
+            getenv('ZOHO_RESOURCE_PATH')
+        );
+
+        $line = yield;
+        do {
+            $body = new \com\zoho\crm\api\record\BodyWrapper();
+            $body->setData(
+                [
+                    'Product Name' => 'Test',
+                    'Product Code' => 'TEST',
+                    'Unit Price' => '1.00',
+                    'Description' => 'Test',
+                ]
+            );
+
+            (new \com\zoho\crm\api\record\RecordOperations())
+                ->upsertRecords(
+                    'Contacts',
+                    $body,
+                );
+        } while ($line = yield new \Kiboko\Component\Bucket\AcceptanceResultBucket($line));
+    }
+}
---
+++ src/ProductLoader.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+use Kiboko\Contract\Pipeline\LoaderInterface;
+
+final class ProductLoader implements LoaderInterface
+{
+    public function load(): \Generator
+    {
+        $token = new \com\zoho\api\authenticator\OAuthToken(
+            getenv('ZOHO_CLIENT_ID'),
+            getenv('ZOHO_CLIENT_SECRET'),
+            getenv('ZOHO_ACCESS_TOKEN'),
+            getenv('ZOHO_REFRESH_TOKEN'),
+        );
+
+        $config = (new \com\zoho\crm\api\SDKConfigBuilder())->build();
+
+        \com\zoho\crm\api\Initializer::initialize(
+            new \com\zoho\crm\api\UserSignature(
+                getenv('ZOHO_USER_EMAIL'),
+            ),
+            \com\zoho\crm\api\dc\EUDataCenter::PRODUCTION(),
+            $token,
+            new \com\zoho\api\authenticator\store\FileStore('/tmp/zoho_token.txt'),
+            $config,
+            getenv('ZOHO_RESOURCE_PATH')
+        );
+
+        $line = yield;
+        do {
+            $body = new \com\zoho\crm\api\record\BodyWrapper();
+
+            $productLine =  new \com\zoho\crm\api\record\LineItemProduct();
+            $productLine->setName('Test');
+
+            $body->setData(
+                [
+                   $productLine,
+                ]
+            );
+
+            (new \com\zoho\crm\api\record\RecordOperations())
+                ->upsertRecords(
+                    'Products',
+                    $body,
+                );
+        } while ($line = yield new \Kiboko\Component\Bucket\AcceptanceResultBucket($line));
+    }
+}
